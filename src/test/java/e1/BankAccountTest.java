29a0521729268f4e50caf15d99512b76c938fced
MarcoCostantini26 <marco.costantini26@gmail.com>
2025-03-04 09:20:56 +0100

    refactoring magical numbers and implement testCannotWithdrawMoreThanAvailableInBronzeAccount

--- src/test/java/e1/BankAccountTest.java
+++ src/test/java/e1/BankAccountTest.java
@@ -10,6 +10,16 @@ public class BankAccountTest {
     public static final int DEPOSIT_INITIAL_BALANCE = 1000;
     public static final int WITHDRAW_LESS_THAN_100 = 90;
     private static final int WITHDRAW_MORE_THAN_100 = 110;
+    public static final int INITIAL_BALANCE = 0;
+    public static final int AMOUNT_200_MONEY = 200;
+    public static final int AMOUNT_799_MONEY = 799;
+    public static final int AMOUNT_1200_MONEY = 1200;
+    public static final int AMOUNT_10_MONEY = 10;
+    public static final int AMOUNT_990_MONEY = 990;
+    public static final int WITHDRAW_FOR_NEGATIVE_BALANCE = 1500;
+    public static final int NEGATIVE_BALANCE = -500;
+    public static final int AMOUNT_910_MONEY = 910;
+    public static final int AMOUNT_899_MONEY = 889;
     private SilverBankAccount account;
     private GoldBankAccount goldAccount;
     private BronzeBankAccount bronzeAccount;
@@ -23,7 +33,7 @@ public class BankAccountTest {
 
     @Test
     public void testInitiallyEmpty() {
-        assertEquals(0, this.account.getBalance());
+        assertEquals(INITIAL_BALANCE, this.account.getBalance());
     }
 
     @Test
@@ -35,30 +45,30 @@ public class BankAccountTest {
     @Test
     public void testCanWithdraw() {
         this.account.deposit(DEPOSIT_INITIAL_BALANCE);
-        this.account.withdraw(200);
-        assertEquals(799, this.account.getBalance());
+        this.account.withdraw(AMOUNT_200_MONEY);
+        assertEquals(AMOUNT_799_MONEY, this.account.getBalance());
     }
 
     @Test
     public void testCannotWithdrawMoreThanAvailable(){
         this.account.deposit(DEPOSIT_INITIAL_BALANCE);
-        assertThrows(IllegalStateException.class, () -> this.account.withdraw(1200));
+        assertThrows(IllegalStateException.class, () -> this.account.withdraw(AMOUNT_1200_MONEY));
     }
 
     @Test
     public void testGoldAccountFee(){
         this.goldAccount.deposit(DEPOSIT_INITIAL_BALANCE);
-        this.goldAccount.withdraw(10);
-        assertEquals(990, this.goldAccount.getBalance());
+        this.goldAccount.withdraw(AMOUNT_10_MONEY);
+        assertEquals(AMOUNT_990_MONEY, this.goldAccount.getBalance());
     }
 
     @Test
     public void testNegativeBalanceGoldAccount(){
         this.goldAccount.deposit(DEPOSIT_INITIAL_BALANCE);
-        this.goldAccount.withdraw(1500);
+        this.goldAccount.withdraw(WITHDRAW_FOR_NEGATIVE_BALANCE);
         assertAll(
-                () -> assertEquals(-500, goldAccount.getBalance()),
-                () -> assertThrows(IllegalStateException.class, () -> goldAccount.withdraw(500))
+                () -> assertEquals(NEGATIVE_BALANCE, goldAccount.getBalance()),
+                () -> assertThrows(IllegalStateException.class, () -> goldAccount.withdraw(WITHDRAW_FOR_NEGATIVE_BALANCE))
         );
     }
 
@@ -66,16 +76,20 @@ public class BankAccountTest {
     public void testBronzeAccountFeeLessThanOverdraft(){
         this.bronzeAccount.deposit(DEPOSIT_INITIAL_BALANCE);
         this.bronzeAccount.withdraw(WITHDRAW_LESS_THAN_100);
-        assertEquals(910, this.bronzeAccount.getBalance());
+        assertEquals(AMOUNT_910_MONEY, this.bronzeAccount.getBalance());
     }
 
     @Test
     public void testBronzeAccountFeeMoreThanOverdraft(){
         this.bronzeAccount.deposit(DEPOSIT_INITIAL_BALANCE);
         this.bronzeAccount.withdraw(WITHDRAW_MORE_THAN_100);
-        assertEquals(889, this.bronzeAccount.getBalance());
+        assertEquals(AMOUNT_899_MONEY, this.bronzeAccount.getBalance());
     }
 
-
+    @Test
+    public void testCannotWithdrawMoreThanAvailableInBronzeAccount(){
+        this.bronzeAccount.deposit(DEPOSIT_INITIAL_BALANCE);
+        assertThrows(IllegalStateException.class, () -> this.bronzeAccount.withdraw(AMOUNT_1200_MONEY));
+    }
 
 }
